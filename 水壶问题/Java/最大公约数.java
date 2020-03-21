c14113d1631f3f37b372a243848e06a08450f40c
张炎华 <zhangyh2360@dimpt.com>
2020-03-21 23:16:31 +0800

    添加水壶问题的搜索解法，待实现

--- 水壶问题/Java/最大公约数.java
+++ 水壶问题/Java/最大公约数.java
@@ -3,6 +3,7 @@ class Solution {
      * 问题可以转花为找到m和n使得mx + ny = k，其中m，n可以为正数或负数。
      * 对于这类问题，数学上可以证明只要x，y的最大公约数能整除k，就存在一对
      * m，n实现mx + ny = k。 因此问题就转化为寻找x,y的最大公约数是否能整除k。
+     * 贝祖定理
      */
     public boolean canMeasureWater(int x, int y, int z) {
         if (z == 0) {
